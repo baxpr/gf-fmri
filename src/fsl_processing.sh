f8202e303b9111eaa55f86ace2a7669125607e7b
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2021-02-11 21:11:10 -0600

    fwd-rev

--- src/fsl_processing.sh
+++ src/fsl_processing.sh
@@ -1,20 +1,19 @@
 #!/usr/bin/env bash
 
-# TODO recharacterize PE as fwd/rev and have an input for +/- i,j,k axis
-# Keep fwd and discard rev. Sanity check to make sure fwd isn't the short one
+# TODO Sanity check to make sure FWD isn't the short one
 
 # Inputs
 t1_niigz=../INPUTS/t1.nii.gz
-fmriAPA_niigz=../INPUTS/fmri_APA.nii.gz
-fmriAPP_niigz=../INPUTS/fmri_APP.nii.gz
+fmriFWD_niigz=../INPUTS/fmri_FWD.nii.gz
+fmriREV_niigz=../INPUTS/fmri_REV.nii.gz
 #seg_niigz=../INPUTS/seg.nii.gz
 pedir="+j"
 
 # Copy files to working dir
 wkdir=../OUTPUTS
 cp "${t1_niigz}" "${wkdir}"/t1.nii.gz
-cp "${fmriAPA_niigz}" "${wkdir}"/apa.nii.gz
-cp "${fmriAPP_niigz}" "${wkdir}"/app.nii.gz
+cp "${fmriFWD_niigz}" "${wkdir}"/fwd.nii.gz
+cp "${fmriREV_niigz}" "${wkdir}"/rev.nii.gz
 #cp "${seg_niigz}" "${wkdir}"/seg.nii.gz
 
 # Get in wkdir
@@ -38,11 +37,11 @@ bet t1 t1brain -R -f 0.5 -m
 #rm tmp.nii.gz
 
 # Motion
-mcflirt -in apa -meanvol -out rapa
-mcflirt -in app -meanvol -out rapp
+mcflirt -in fwd -meanvol -out rfwd
+mcflirt -in rev -meanvol -out rrev
 
 # Combine mean fMRIs to make topup input with two vols
-fslmerge -t topup rapa_mean_reg rapp_mean_reg
+fslmerge -t topup rfwd_mean_reg rrev_mean_reg
 
 # Make topup phase dir info file
 make_datain.sh "${pedir}"
@@ -52,34 +51,34 @@ make_datain.sh "${pedir}"
 topup --imain=topup --datain=datain.txt --fout=topup_fieldmap_hz --config=b02b0_1.cnf
 
 # Apply topup to APA (this is the one we will actually use)
-applytopup --imain=rapa_mean_reg --inindex=1 --datain=datain.txt \
-	--topup=topup --out=trapa_mean_reg --method=jac
+applytopup --imain=rfwd_mean_reg --inindex=1 --datain=datain.txt \
+	--topup=topup --out=trfwd_mean_reg --method=jac
 
 # Apply topup to APP - just for reference
-applytopup --imain=rapp_mean_reg --inindex=1 --datain=datain.txt \
-	--topup=topup --out=trapp_mean_reg --method=jac
+applytopup --imain=rrev_mean_reg --inindex=2 --datain=datain.txt \
+	--topup=topup --out=trrev_mean_reg --method=jac
 
 # Register to T1
-epi_reg --epi=trapa_mean_reg --t1=t1 --t1brain=t1brain --out=ctrapa_mean_reg
-#epi_reg --epi=trapa_mean_reg --t1=t1 --t1brain=t1brain --wmseg=wm --out=ctrapa_mean_reg
+epi_reg --epi=trfwd_mean_reg --t1=t1 --t1brain=t1brain --out=ctrfwd_mean_reg
+#epi_reg --epi=trfwd_mean_reg --t1=t1 --t1brain=t1brain --wmseg=wm --out=ctrfwd_mean_reg
 
 # Register to T1 without topup - just for reference
-epi_reg --epi=rapa_mean_reg --t1=t1 --t1brain=t1brain --wmseg=ctrapa_mean_reg_fast_wmseg --out=crapa_mean_reg
-#epi_reg --epi=rapa_mean_reg --t1=t1 --t1brain=t1brain --wmseg=wm --out=crapa_mean_reg
+epi_reg --epi=rfwd_mean_reg --t1=t1 --t1brain=t1brain --wmseg=ctrfwd_mean_reg_fast_wmseg --out=crfwd_mean_reg
+#epi_reg --epi=rfwd_mean_reg --t1=t1 --t1brain=t1brain --wmseg=wm --out=crfwd_mean_reg
 
 # Apply topup to actual time series
-applytopup --imain=rapa --inindex=1 --datain=datain.txt --topup=topup --out=trapa --method=jac
+applytopup --imain=rfwd --inindex=1 --datain=datain.txt --topup=topup --out=trfwd --method=jac
 
 # Apply coregistration to the topup-corrected time series
-flirt -applyisoxfm 1.5 -init ctrapa_mean_reg.mat -in trapa -ref ctrapa_mean_reg -out ctrapa
+flirt -applyisoxfm 1.5 -init ctrfwd_mean_reg.mat -in trfwd -ref ctrfwd_mean_reg -out ctrfwd
 
 # Apply coreg to the mean topup corrected fmriAPP - just for reference. Same matrix as for APA
-flirt -applyisoxfm 1.5 -init ctrapa_mean_reg.mat -in trapp_mean_reg -ref ctrapa_mean_reg \
-	-out ctrapp_mean_reg
+flirt -applyisoxfm 1.5 -init ctrfwd_mean_reg.mat -in trrev_mean_reg -ref ctrfwd_mean_reg \
+	-out ctrrev_mean_reg
 
 # Give things more meaningful filenames
-mv ctrapa_mean_reg.nii.gz coregistered_mean_fmriAPA.nii.gz
-mv ctrapp_mean_reg.nii.gz coregistered_mean_fmriAPP.nii.gz
-mv ctrapa.nii.gz coregistered_fmriAPA.nii.gz
-mv crapa_mean_reg.nii.gz coregistered_mean_fmriAPA_notopup.nii.gz
+mv ctrfwd_mean_reg.nii.gz coregistered_mean_fmriFWD.nii.gz
+mv ctrrev_mean_reg.nii.gz coregistered_mean_fmriREV.nii.gz
+mv ctrfwd.nii.gz coregistered_fmriFWD.nii.gz
+mv crfwd_mean_reg.nii.gz coregistered_mean_fmriFWD_no_topup.nii.gz
 
