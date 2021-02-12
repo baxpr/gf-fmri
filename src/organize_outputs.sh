d5db98b394f47a367c346bc479511228d62f0f8a
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2021-02-12 17:44:50 -0600

    outs

--- src/organize_outputs.sh
+++ src/organize_outputs.sh
@@ -1,18 +1,21 @@
 #!/usr/bin/env bash
 
 
-## FMRI_TOPUP  - mean fmri images after motion correction and topup but before registration
+## MEANFMRI_TOPUP  - mean fmri images after motion correction and topup but before registration
 # mean_fmriFWD.nii.gz                         FWD mean fmri in native space
 # mean_fmriREV.nii.gz                         REV mean fmri in native space
 
 
-## FMRI_TOPUP_REG - fmri images after topup and registration to t1
+## MEANFMRI_TOPUP_REG - mean fmri images after motion correction, topup, and registration to t1
 # coregistered_mean_fmriFWD.nii.gz            FWD mean fmri in native space
 # coregistered_mean_fmriREV.nii.gz            REV mean fmri in native space
-# coregistered_fmriFWD.nii.gz                 fmri time series
 # mean_fmri_to_t1.mat
 
-## WMSEG - White matter segmentation used with epi_reg
+## FMRI_TOPUP_REG - fmri time series after motion correction, topup, and registration to t1
+# coregistered_fmriFWD.nii.gz                 fmri time series
+
+
+## WMSEG - White matter segmentation of t1 used with epi_reg
 # wm.nii.gz                   White matter seg used for registration
 
 
@@ -25,20 +28,25 @@
 # datain.txt
 
 
-## FMRI_NO_TOPUP - images without topup for comparison
+## PROBABLY DONT NEED THESE
+## MEANFMRI_NO_TOPUP - images after motion correction but without topup or registration, for comparison
 # mean_fmriFWD_no_topup.nii.gz
 # mean_fmriREV_no_topup.nii.gz
 
-## FMRI_REG  - images without topup, registered to t1
+## MEANFMRI_REG  - images without topup, registered to t1. For evaluation of topup
 # coregistered_mean_fmriFWD_no_topup.nii.gz
 # coregistered_mean_fmriREV_no_topup.nii.gz
 # mean_fmriFWD_no_topup_to_t1.mat
 # mean_fmriREV_no_topup_to_t1.mat
 
 
-## FMRI_MNI - fmri images warped to MNI space
+## MEANFMRI_MNI - mean fmri image after motion correction, topup, registration to t1, and warp to MNI space
 # wmeanfmri.nii.gz   Mean FWD fmri in MNI space
+
+## FMRI_MNI - fmri time series after motion correction, topup, registration to t1, and warp to MNI space
 # wfmri.nii.gz       FWD fmri in MNI space
+
+# SFMRI_MNI - smoothed fmri time series after motion correction, topup, registration to t1, and warp to MNI space
 # swfmri.nii.gz      Smoothed FWD fmri in MNI space  (?)
 
 
