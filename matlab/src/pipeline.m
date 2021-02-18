7c92d54258cc6f276da2dc6dfd76ed8969981830
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2021-02-17 21:45:58 -0600

    readtable

--- matlab/src/pipeline.m
+++ matlab/src/pipeline.m
@@ -6,12 +6,12 @@
 % Verify csv read works for all tasks
 
 % Inputs
-deffwd_niigz = '../INPUTS_Oddball/y_t1.nii.gz';
-meanfmri_niigz = '../OUTPUTS_Oddball/coregistered_mean_fmriFWD.nii.gz';
-fmri_niigz = '../OUTPUTS_Oddball/coregistered_fmriFWD.nii.gz';
-eprime_summary_csv = '../INPUTS_Oddball/eprime_summary_Oddball.csv';
-motion_par = '../OUTPUTS_Oddball/rfwd.par';
-out_dir = '../OUTPUTS_Oddball_spm';
+deffwd_niigz = '../../INPUTS_Oddball/y_t1.nii.gz';
+meanfmri_niigz = '../../OUTPUTS_Oddball/coregistered_mean_fmriFWD.nii.gz';
+fmri_niigz = '../../OUTPUTS_Oddball/coregistered_fmriFWD.nii.gz';
+eprime_summary_csv = '../../INPUTS_Oddball/eprime_summary_Oddball.csv';
+motion_par = '../../OUTPUTS_Oddball/rfwd.par';
+out_dir = '../../OUTPUTS_Oddball_spm';
 fwhm = 4;
 hpf = 300;
 task = 'Oddball';
@@ -54,10 +54,12 @@ spm_jobman('run',matlabbatch);
 
 
 %% Save condition info into SPM-style .mat
+% Read the csv once to get the size, then again to specify field format.
+% Matlab is still not clever enough to suss this out on its own
 names = {}; onsets = {}; durations = {};
 ep = readtable([out_dir '/eprime_summary.csv'],'Delimiter','comma');
-%ep = readtable([out_dir '/eprime_summary.csv'], ...
-%	'Format',repmat('%q',1,size(ep,2)));
+ep = readtable([out_dir '/eprime_summary.csv'], ...
+	'Format',repmat('%q',1,size(ep,2)));
 for c = 1:height(ep)
 	names{c,1} = ep.Condition{c};
 	onsets{c,1} = eval(ep.OnsetsSec{c});
