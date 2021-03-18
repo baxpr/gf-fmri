b4852ad191cee5944f11e71c7e2500a252aafc87
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2021-03-18 10:54:28 -0500

    compile

--- test_container.sh
+++ test_container.sh
@@ -1,6 +1,33 @@
 #!/usr/bin/env bash
 
-mkdir OUTPUTS_141620_WM
+
+mkdir -p OUTPUTS_141620_Oddball
+singularity run --contain --cleanenv \
+--home $(pwd)/INPUTS_141620 \
+--bind INPUTS_141620:/INPUTS \
+--bind OUTPUTS_141620_Oddball:/OUTPUTS \
+test.simg \
+--project GenFac_HWZ \
+--subject 141620 \
+--session 141620 \
+--scan Oddball \
+--mt1_niigz /INPUTS/mt1.nii.gz \
+--fmriFWD_niigz /INPUTS/801_oddball1APA_FMRI_MB3_2_5mm_1300.nii.gz \
+--fmriREV_niigz /INPUTS/401_rest2APP_FMRI_MB3_2_5mm_1300.nii.gz \
+--seg_niigz /INPUTS/T1_seg.nii.gz \
+--icv_niigz /INPUTS/p0t1.nii.gz \
+--deffwd_niigz /INPUTS/y_t1.nii.gz \
+--pedir +j \
+--eprime_summary_csv /INPUTS/eprime_summary_OddballOld1.csv \
+--vox_mm 1.5 \
+--run_topup yes \
+--fwhm 4 \
+--hpf 300 \
+--task Oddball \
+--out_dir /OUTPUTS
+
+
+mkdir -p OUTPUTS_141620_WM
 singularity run --contain --cleanenv \
 --home $(pwd)/INPUTS_141620 \
 --bind INPUTS_141620:/INPUTS \
@@ -26,7 +53,7 @@ test.simg \
 --out_dir /OUTPUTS
 
 
-mkdir OUTPUTS_141620_SPT
+mkdir -p OUTPUTS_141620_SPT
 singularity run --contain --cleanenv \
 --home $(pwd)/INPUTS_141620 \
 --bind INPUTS_141620:/INPUTS \
@@ -51,29 +78,4 @@ test.simg \
 --task SPT \
 --out_dir /OUTPUTS
 
-
-mkdir OUTPUTS_141620_Oddball
-singularity run --contain --cleanenv \
---home $(pwd)/INPUTS_141620 \
---bind INPUTS_141620:/INPUTS \
---bind OUTPUTS_141620_Oddball:/OUTPUTS \
-test.simg \
---project GenFac_HWZ \
---subject 141620 \
---session 141620 \
---scan Oddball \
---mt1_niigz /INPUTS/mt1.nii.gz \
---fmriFWD_niigz /INPUTS/801_oddball1APA_FMRI_MB3_2_5mm_1300.nii.gz \
---fmriREV_niigz /INPUTS/401_rest2APP_FMRI_MB3_2_5mm_1300.nii.gz \
---seg_niigz /INPUTS/T1_seg.nii.gz \
---icv_niigz /INPUTS/p0t1.nii.gz \
---deffwd_niigz /INPUTS/y_t1.nii.gz \
---pedir +j \
---eprime_summary_csv /INPUTS/eprime_summary_Oddball1.csv \
---vox_mm 1.5 \
---run_topup yes \
---fwhm 4 \
---hpf 300 \
---task Oddball \
---out_dir /OUTPUTS
 
