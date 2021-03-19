47492540ca334d413ff16a157b90255da827b367
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2021-03-19 18:39:00 -0500

    pdf

--- src/make_pdf.sh
+++ src/make_pdf.sh
@@ -9,12 +9,14 @@ cd ${OUTDIR}
 
 thedate=$(date)
 
+c=10
 for slice in -35 -20 -5 10 25 40 55 70  ; do
-	fsleyes render -of ax_${slice}.png \
+	((c++))
+	fsleyes render -of ax_${c}.png \
 		--scene ortho --worldLoc 0 0 ${slice} --displaySpace world --size 600 600 --yzoom 1000 \
-		--layout horizontal --hideCursor --hideLabels --hidey --hidez \
+		--layout horizontal --hideCursor --hideLabels --hidex --hidey \
 		wmt1 --overlayType volume \
-		spm/spmT_000${CONTRAST} --overlayType volume --displayRange 3.5 10 \
+		spm/spmT_000${CONTRAST} --overlayType volume --displayRange 3 10 \
 		--useNegativeCmap --cmap red-yellow --negativeCmap blue-lightblue
 done
 
@@ -37,10 +39,7 @@ ${MAGICKDIR}/convert -size 2600x3365 xc:white \
 
 ${MAGICKDIR}/convert -size 2600x3365 xc:white \
 	-gravity center \( first_level_design_001.png -resize 2400x \) -composite \
-	-gravity North -pointsize 48 -annotate +0+100 \
-	"$TASK fMRI design" \
 	-gravity SouthEast -pointsize 48 -annotate +100+100 "${thedate}" \
-	-gravity NorthWest -pointsize 48 -annotate +100+200 "${info_string}" \
 	first_level_design_001.png
 
 ${MAGICKDIR}/convert page_ax.png first_level_design_001.png gf-fmri.pdf
