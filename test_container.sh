02fa31f1c197a609b33f7f64e6f70f3936582e7b
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2021-03-21 11:26:04 -0500

    compiled

--- test_container.sh
+++ test_container.sh
@@ -1,5 +1,33 @@
 #!/usr/bin/env bash
 
+mkdir -p OUTPUTS_141620_WM
+singularity run --contain --cleanenv \
+--home $(pwd)/INPUTS_141620 \
+--bind INPUTS_141620:/INPUTS \
+--bind OUTPUTS_141620_WM:/OUTPUTS \
+test.simg \
+--project GenFac_HWZ \
+--subject 141620 \
+--session 141620 \
+--scan WM1 \
+--mt1_niigz /INPUTS/mt1.nii.gz \
+--wmt1_niigz /INPUTS/wmt1.nii.gz \
+--fmriFWD_niigz /INPUTS/501_wm1APA_FMRI_MB3_2_5mm_1300.nii.gz \
+--fmriREV_niigz /INPUTS/401_rest2APP_FMRI_MB3_2_5mm_1300.nii.gz \
+--seg_niigz /INPUTS/T1_seg.nii.gz \
+--icv_niigz /INPUTS/p0t1.nii.gz \
+--deffwd_niigz /INPUTS/y_t1.nii.gz \
+--pedir +j \
+--eprime_summary_csv /INPUTS/eprime_summary_WM1.csv \
+--vox_mm 1.5 \
+--run_topup yes \
+--fwhm 4 \
+--hpf 300 \
+--task WM \
+--out_dir /OUTPUTS
+
+exit 0 
+
 
 mkdir -p OUTPUTS_141620_Oddball
 singularity run --contain --cleanenv \
@@ -27,32 +55,6 @@ test.simg \
 --task Oddball \
 --out_dir /OUTPUTS
 
-
-mkdir -p OUTPUTS_141620_WM
-singularity run --contain --cleanenv \
---home $(pwd)/INPUTS_141620 \
---bind INPUTS_141620:/INPUTS \
---bind OUTPUTS_141620_WM:/OUTPUTS \
-test.simg \
---project GenFac_HWZ \
---subject 141620 \
---session 141620 \
---scan WM1 \
---mt1_niigz /INPUTS/mt1.nii.gz \
---wmt1_niigz /INPUTS/wmt1.nii.gz \
---fmriFWD_niigz /INPUTS/501_wm1APA_FMRI_MB3_2_5mm_1300.nii.gz \
---fmriREV_niigz /INPUTS/401_rest2APP_FMRI_MB3_2_5mm_1300.nii.gz \
---seg_niigz /INPUTS/T1_seg.nii.gz \
---icv_niigz /INPUTS/p0t1.nii.gz \
---deffwd_niigz /INPUTS/y_t1.nii.gz \
---pedir +j \
---eprime_summary_csv /INPUTS/eprime_summary_WM1.csv \
---vox_mm 1.5 \
---run_topup yes \
---fwhm 4 \
---hpf 300 \
---task WM \
---out_dir /OUTPUTS
 
 
 mkdir -p OUTPUTS_141620_SPT
