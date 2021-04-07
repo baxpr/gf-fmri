7ee573574e970b7062a0cf3a6f672af2f7401d2b
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2021-04-06 20:58:44 -0500

    compiled

--- test_container.sh
+++ test_container.sh
@@ -1,5 +1,34 @@
 #!/usr/bin/env bash
 
+mkdir -p OUTPUTS_141394_SPT
+singularity run --contain --cleanenv \
+--home $(pwd)/INPUTS_141394 \
+--bind INPUTS_141394:/INPUTS \
+--bind OUTPUTS_141394_SPT:/OUTPUTS \
+test.simg \
+--project GenFac_HWZ \
+--subject 141394 \
+--session 141394 \
+--scan SPT \
+--mt1_niigz /INPUTS/mt1.nii.gz \
+--wmt1_niigz /INPUTS/wmt1.nii.gz \
+--fmriFWD_niigz /INPUTS/1001_spt1APA_FMRI_MB3_2_5mm_1300.nii.gz \
+--fmriREV_niigz /INPUTS/401_rest2APP_FMRI_MB3_2_5mm_1300.nii.gz \
+--seg_niigz /INPUTS/T1_seg.nii.gz \
+--icv_niigz /INPUTS/p0t1.nii.gz \
+--deffwd_niigz /INPUTS/y_t1.nii.gz \
+--pedir +j \
+--eprime_summary_csv /INPUTS/eprime_summary_SPT.csv \
+--vox_mm 2 \
+--run_topup yes \
+--fwhm 4 \
+--hpf 300 \
+--task SPT \
+--out_dir /OUTPUTS
+
+
+exit 0
+
 
 mkdir -p OUTPUTS_141620_Oddball
 singularity run --contain --cleanenv \
